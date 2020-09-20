be3d492854ad7bfbad57ad240fdc6ae4a722710f
Peterson Rolim de Freitas <peterrfreitas@gmail.com>
2020-09-19 22:28:36 -0300

    Calculadora IMC em PHP - Alteração da posição do resultado

--- formulario.php
+++ formulario.php
@@ -29,17 +29,17 @@
 	 	 <title>IMC</title>
 </head>
 <body>
- <?php
-    if(isset($imc["valor"])){
-        echo "<h3>O usuário ".$arr["nome"]." com peso ".$arr["peso"]." e altura ".$arr["altura"]."</h3>";
-        echo "<h3>Foi classificado como ".$imc["classificacao"]." Grau de obesidade ".$imc["grau"]."</h3>";
-    }
- ?>
  <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
  Nome: <input type="text" name="nome"><br>
  Altura: <input type="number" name="altura"><br>
  Peso: <input type="number" name="peso"><br>
  <input type="submit">
  </form>
+ <?php
+    if(isset($imc["valor"])){
+        echo "<h3>O usuário ".$arr["nome"]." com peso ".$arr["peso"]." e altura ".$arr["altura"]."</h3>";
+        echo "<h3>Foi classificado como ".$imc["classificacao"]." Grau de obesidade ".$imc["grau"]."</h3>";
+    }
+ ?>
 </body>
 </html>
